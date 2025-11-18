aee3740b64dd04a31bb2864a7b35bd4ee74744db
Tom De Dobbeleer <tdedobbeleer@gmail.com>
2025-11-18 11:44:23 +0100

    Add funny messages for drivers.

--- components/GetPolly.vue
+++ components/GetPolly.vue
@@ -114,7 +114,7 @@
                 class="mb-3"
                 :variant="getProgressVariant(driver.consumers?.length || 0, driver.spots || 0)"
                 :value="countProgress(driver.consumers?.length || 0, driver.spots || 0)"/>
-              <p v-if="(driver.consumers?.length || 0) >= (driver.spots || 0)" class="text-danger mb-3">All spots are filled!</p>
+              <p v-if="(driver.consumers?.length || 0) >= (driver.spots || 0)" class="text-danger mb-3">All spots are filled!<br/>{{ getRandomFunnyMessage() }}</p>
             </BCardHeader>
             <BCardBody>
               <BListGroup v-if="driver.consumers?.length && driver.consumers?.length > 0">
@@ -440,6 +440,26 @@ const startEditingTitle = () => {
 const cancelEditingTitle = () => {
   isEditingTitle.value = false
   editingTitle.value = ''
+}
+
+const funnyMessages = [
+  "Who will call shotgun first?",
+  "I hope it's not too cramped",
+  "The thinnest sits in the middle!",
+  "The oldest chooses the spot first!",
+  "I hope the trunk can handle the junk.",
+  "Don't forget to go to the bathroom before you leave.",
+  "I hope it's a SUV.",
+  "Fasten your seatbelts! The driver is responsible.",
+  "Please, keep the car clean. It's like a house. Don't go in with dirty shoes.",
+  "Let's prepare a singalong playlist! That's always nice.",
+  "Keep quiet in the back, the driver needs to concentrate.",
+  "If you don't keep quiet, you can resume the rest of the drive on foot!",
+  "Don't fight over the Nintendo, everyone get's a turn."
+]
+
+const getRandomFunnyMessage = () => {
+  return funnyMessages[Math.floor(Math.random() * funnyMessages.length)]
 }
 
 const saveTitle = async () => {
