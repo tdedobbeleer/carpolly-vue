d1723dc6a1f40bc6a80fac4a0ea139b8ac904859
Tom De Dobbeleer <tdedobbeleer@gmail.com>
2025-11-24 16:21:31 +0100

    add gtag for google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createGtag } from 'vue-gtag'
 import './firebase'
 
 import 'bootswatch/dist/sketchy/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(createGtag({ tagId: 'G-6HH8MQN7HB' })).mount('#app')
